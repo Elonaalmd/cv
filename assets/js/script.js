ec79f8e54cca23bd01a2a6d77927acdf8784585e
Elona <elona.almeida.sja@gmail.com>
2024-11-19 10:15:11 +0100

    test de la page portfolio, mais je pense partir sur une autre idée

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,9 @@
+//Création du script pour le menu burger.
+
 function toggleMenu() {
     const menu = document.getElementById("menu");
     menu.classList.toggle("show");
 }
+
+//Création du script pour les carrousels.
+
